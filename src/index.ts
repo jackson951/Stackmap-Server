c125c24de6532c96ac33e8efa4d1e4bba9381f1a
Jackson Khuto <jackson951@gmail.com>
2026-03-20 13:33:57 +0200

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT ?? 5000);
-const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:3000";
+const frontendUrl = process.env.FRONTEND_URL ??"https://stackmap-8ipx.vercel.app";
 
 app.use(cors({ origin: frontendUrl }));
 app.use(express.json());
